6e8581714f68305b0e4cd0df8cdf202ec4a48f1c
Vladimir <vladimir.kondrat@rambler.ru>
2021-10-04 12:27:25 +0300

    project complete

---
+++ src/components/Movie.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Movie(props) {
+
+    const { 
+        Title: title, 
+        Year: year, 
+        imdbID: id, 
+        Type: type, 
+        Poster: poster
+    } = props;
+
+
+    return (
+        <div className="card movie">
+            <div className="card-image waves-effect waves-block waves-light">
+                {
+                    poster === 'N/A' ?
+                        <img classNameName="activator" src={`https://via.placeholder.com/300x450?text=${title}`}/>
+                        :
+                        <img classNameName="activator movie-img" src={poster}/> 
+                }
+            </div>
+            <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">{title}</span>                    
+                <p>{year}</p><span className="right">{type}</span>
+            </div>
+        </div>
+    )
+}
+export { Movie }
---
+++ src/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Footer() {
+    return (
+        <footer className="page-footer purple lighten-1">
+            <div class="footer-copyright">
+                <div className="container">
+                    © {new Date().getFullYear()} Copyright Text
+                    <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
